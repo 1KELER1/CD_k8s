3b8251cb13b242c86196da61baac6bc6801658ec
Victor Keler <victor@keler.com>
2026-02-05 18:47:59 +0800

    тест линтеров

--- app/main.py
+++ app/main.py
@@ -1,20 +1,24 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 # Монтируем статические файлы
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
 @app.get("/")
 async def get_home():
     return FileResponse("static/index.html")
+
 
 @app.get("/api/hello")
 async def hello():
     return {"message": "Hello World!"}
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
