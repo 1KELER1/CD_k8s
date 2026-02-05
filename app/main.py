6be228ffff7f04140bd43f3acc09606b2cd0df1a
Victor Keler <victor@keler.com>
2026-02-05 19:00:24 +0800

    ожидаю ожидаю успешную проверку

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-import uvicorn
 
 app = FastAPI()
 
 # Монтируем статические файлы
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
 @app.get("/")
 async def get_home():
     return FileResponse("static/index.html")
+
 
 @app.get("/api/hello")
 async def hello():
